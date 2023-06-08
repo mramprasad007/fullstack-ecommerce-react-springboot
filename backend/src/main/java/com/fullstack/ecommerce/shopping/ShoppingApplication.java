c98a8422310e66f9a417a4c1cc42b85672aac04f
Ram Prasad <mramprasad007@gmail.com>
2023-06-08 11:57:09 +0530

    initial commit

--- backend/src/main/java/com/fullstack/ecommerce/shopping/ShoppingApplication.java
+++ backend/src/main/java/com/fullstack/ecommerce/shopping/ShoppingApplication.java
@@ -1,9 +1,19 @@
 package com.fullstack.ecommerce.shopping;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import org.springframework.boot.SpringApplication;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition
+@SecurityScheme(
+    name = "bearerAuth",
+    type = SecuritySchemeType.HTTP,
+    bearerFormat = "JWT",
+    scheme = "bearer"
+)
 public class ShoppingApplication {
 
 	public static void main(String[] args) {
