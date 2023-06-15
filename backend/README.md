ac03208edc1080ac680aac8e964a59991ca2a06e
Ram Prasad <mramprasad007@gmail.com>
2023-06-15 13:51:18 +0530

    initial commit

--- backend/README.md
+++ backend/README.md
@@ -2,14 +2,6 @@
 
 ## Database
 
-### Local DB Setup
-Make sure Docker is running and run the below commend to start up the db:
-
-```bash
-# runs a brand new instance of the database
-$ docker run --name testdb -p 5432:5432 -e POSTGRES_PASSWORD=mysecretpassword -d postgres:15.3
-```
-
 ### Create db schema
 ```
 CREATE SCHEMA shopping
--- backend/src/main/java/com/fullstack/ecommerce/shopping/api/Health.java
+++ backend/src/main/java/com/fullstack/ecommerce/shopping/api/Health.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,10 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class Health {
     @Autowired
 
+    private Environment environment;
     @GetMapping("/health")
     public Map<String, Object> health(){
+        String dbUrl = environment.getProperty("spring.datasource.url");
         final Map<String, Object> response = new HashMap<>();
         response.put("status", "healthy");
+        response.put("dbUrl", dbUrl);
         return response;
     }
 }
